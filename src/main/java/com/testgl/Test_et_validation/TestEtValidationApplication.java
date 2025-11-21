bb922633e222ddeba9fc1d385f8a42c6227e6f12
Beautero-ks <beauterokenne37@gmail.com>
2025-11-21 16:46:56 +0100

    first commit

---
+++ src/main/java/com/testgl/Test_et_validation/TestEtValidationApplication.java
@@ -0,0 +1,13 @@
+package com.testgl.Test_et_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEtValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEtValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testgl/Test_et_validation/TestEtValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testgl.Test_et_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEtValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
